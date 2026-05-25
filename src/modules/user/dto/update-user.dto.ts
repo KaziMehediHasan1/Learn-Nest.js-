dbdc82a48fb01db32e05a11a674812852fd1cc01
KaziMehediHasan1 <kazimehedihasan.tech@gmail.com>
2026-05-25 10:47:14 +0600

    finished updated user model operation

--- src/modules/user/dto/update-user.dto.ts
+++ src/modules/user/dto/update-user.dto.ts
@@ -1,4 +1,18 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsString } from 'class-validator';
+import { Role } from '@prisma/client';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsString()
+  name?: string;
+
+  @IsString()
+  email?: string;
+
+  @IsString()
+  password!: string;
+
+  @IsString()
+  role!: Role;
+}
